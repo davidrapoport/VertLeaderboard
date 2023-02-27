bda07feb6303f0567c79d1f0e8bb7121e97c3bc6
David Rapoport <drapoport847@gmail.com>
2023-02-27 12:24:31 -0700

    Deploy

--- vert-leaderboard/src/firebase.js
+++ vert-leaderboard/src/firebase.js
@@ -15,7 +15,7 @@ import { firebaseConfig } from "./firebaseConfig";
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-connectFirestoreEmulator(db, "localhost", 8080);
+// connectFirestoreEmulator(db, "localhost", 8080);
 
 const getUserFromWebId = async (webId) => {
   const q = query(collection(db, "users"), where("webId", "==", webId));
